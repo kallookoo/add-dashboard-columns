1df8c80e59a8bbd67a3c12ff77913da868deb8b6
kallookoo <747817+kallookoo@users.noreply.github.com>
2019-04-14 14:10:51 +0200

    Version 1.3.5

--- add-dashboard-columns.php
+++ add-dashboard-columns.php
@@ -3,35 +3,112 @@
  * Plugin Name: Add Dashboard Columns
  * Plugin URI: http://wordpress.org/plugins/add-dashboard-columns/
  * Description: Enable Dashboard Columns in WordPress after version 3.8
- * Version: 1.3.4.2
+ * Version: 1.3.5
  * Author: Sergio P.A. ( 23r9i0 )
  * Author URI: http://dsergio.com/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: add-dashboard-columns
+ * Domain Path: /languages
  */
 
 defined( 'ABSPATH' ) or exit;
 
-add_action( 'admin_head-index.php', function() {
-	add_screen_option( 'layout_columns', array( 'max' => 4, 'default' => 3 ) );
-	echo '<style id="add-dashboard-columns" type="text/css">' .
-		'#wpbody #wpbody-content #dashboard-widgets .postbox-container{width:100%}' .
-		'@media (min-width:768px){' .
-			'#wpbody #wpbody-content #dashboard-widgets:not(.columns-1) .postbox-container{width:50%}' .
-		'}' .
-		'@media (min-width:1200px){' .
-			'#wpbody #wpbody-content #dashboard-widgets.columns-3 .postbox-container{width:33.3%}' .
-			'#wpbody #wpbody-content #dashboard-widgets.columns-4 .postbox-container{width:25%}' .
-		'}' .
-		'</style>';
-}, 12 );
+final class Add_Dashboard_Columns {
 
-add_filter( 'plugin_row_meta', function( $plugin_meta, $plugin_file, $plugin_data, $status ) {
-	if( $plugin_file === plugin_basename( __FILE__ ) ) {
-		$plugin_meta[] = sprintf(
-			'<a target="_blank" href="%s">%s</a>',
-			esc_url( 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=L4BFVU5HDJH8S' ), __( 'Donate' )
-		);
+	private static $instance;
+
+	private $plugin_file;
+
+	private $is_plugin_network;
+
+	public static function admin_init() {
+		return isset( self::$instance ) ? self::$instance : self::$instance = new self;
 	}
-	return $plugin_meta;
-}, 10, 4 );
+
+	private function __clone() {}
+
+	private function __wakeup() {}
+
+	private function __construct() {
+		load_plugin_textdomain( 'add-dashboard-columns', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+
+		add_action( 'admin_head-index.php', array( $this, 'admin_head' ), 12 );
+		add_filter( 'plugin_row_meta', array( $this, 'plugin_row_meta' ), 10, 4 );
+	}
+
+	public function admin_head() {
+		add_screen_option( 'layout_columns', array( 'max' => 4, 'default' => 3 ) );
+		echo '<style id="add-dashboard-columns" type="text/css">' .
+			'#wpbody #wpbody-content #dashboard-widgets .postbox-container{width:100%}' .
+			'@media (min-width:768px){' .
+				'#wpbody #wpbody-content #dashboard-widgets:not(.columns-1) .postbox-container{width:50%}' .
+			'}' .
+			'@media (min-width:1200px){' .
+				'#wpbody #wpbody-content #dashboard-widgets.columns-3 .postbox-container{width:33.3%}' .
+				'#wpbody #wpbody-content #dashboard-widgets.columns-4 .postbox-container{width:25%}' .
+			'}' .
+			'</style>';
+	}
+
+	public function plugin_row_meta( $plugin_meta, $plugin_file, $plugin_data, $status ) {
+		if ( $plugin_file === plugin_basename( __FILE__ ) ) {
+			$plugin_meta[] = sprintf(
+				'<a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=L4BFVU5HDJH8S">%s</a>',
+				__( 'Make a donation', 'add-dashboard-columns' )
+			);
+		}
+
+		return $plugin_meta;
+	}
+
+	public static function deactivation( $network_wide = false ) {
+		$capability = $network_wide ? 'manage_network_plugins' : 'activate_plugins';
+		if ( current_user_can( $capability ) ) {
+
+			global $action;
+
+			switch ( (string) $action ) {
+				case 'activate':
+				case 'deactivate':
+					if ( isset( $_REQUEST['plugin'] ) ) {
+						$referer = "{$action}-plugin_{$_REQUEST['plugin']}";
+					}
+					break;
+				case 'activate-selected':
+				case 'deactivate-selected':
+					$plugin  = plugin_basename( __FILE__ );
+					$plugins = isset( $_POST['checked'] ) ? (array) $_POST['checked'] : array();
+
+					if ( in_array( $plugin, $plugins ) ) {
+						$referer = 'bulk-plugins';
+					}
+					break;
+				default:
+					$referer = '';
+					break;
+			}
+
+			check_admin_referer( $referer );
+
+			global $wpdb;
+			$users_id = $wpdb->get_col( $wpdb->prepare(
+				"SELECT DISTINCT user_id FROM $wpdb->usermeta WHERE meta_key LIKE '%s'",
+				'%' . $wpdb->esc_like( 'screen_layout_dashboard' ) . '%'
+			) );
+
+			if ( $total = count( $users_id ) ) {
+				// http://stackoverflow.com/a/10634225
+				$prepare_in_array = implode( ', ', array_fill( 0, $total, '%d' ) );
+				$wpdb->query( $wpdb->prepare(
+					"DELETE FROM $wpdb->usermeta WHERE user_id IN ({$prepare_in_array}) AND meta_key LIKE '%s'",
+					array_merge( $users_id, array( '%' . $wpdb->esc_like( 'screen_layout_dashboard' ) . '%' ) )
+				) );
+			}
+
+		}
+	}
+}
+
+add_action( 'admin_init', 'Add_Dashboard_Columns::admin_init', 10 );
+register_deactivation_hook( __FILE__, 'Add_Dashboard_Columns::deactivation' );
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Add Dashboard Columns ===
 Contributors: 23r9i0
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=L4BFVU5HDJH8S
-Tags: dashboard, widget, dashboard columns, columns
+Tags: dashboard, widget, dashboard columns, columns, admin
 Requires at least: 3.8
 Tested up to: 4.6
-Stable tag: 1.3.4.2
+Stable tag: 1.3.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -19,24 +19,43 @@ Enable Dashboard Columns in WordPress 3.8 or later
 * Upload the 'add-dashboard-columns' folder to the '/wp-content/plugins/' directory.
 * Activate the plugin through the 'Plugins' menu in WordPress.
 
+== Frequently Asked Questions ==
+
+= What meta delete? =
+This plugin only delete screen_layout_dashboard and screen_layout_dashboard-network if enabled for network
+
+= Why not delete meta-box-order_dashboard or meta-box-order_dashboard-network? =
+Basically because I do not consider it necessary, and may be an established order has not been changed and the number of columns.
+
 == Changelog ==
+
+= 1.3.5 =
+* Rewrite plugin
+* Delete meta on plugin deactivation ( Tested with 10000 users with meta, single installation and multisite installation )
+
 = 1.3.4.2 =
 * Remove register_deactivation_hook, because on many users it may not work properly
+
 = 1.3.4.1 =
 * Fix WP CLI
+
 = 1.3.4 =
 * Rewrite plugin
 * Delete in WordPress 4.6+ screen_layout_dashboard, meta-box-order_dashboard,
   also delete if multisite screen_layout_dashboard-network, meta-box-order_dashboard-network on deactivation
+
 = 1.3.3 =
 * Check support
 * Use bootstrap media for clean CSS
 * Remove admin_enqueue_script, now uses admin_head-index.php to print Custom CSS
+
 = 1.3.2 =
 * Check support
 * Add donation link in plugin page
+
 = 1.3.1 =
 * Check support
+
 = 1.3 =
 * Check support
 * clean directories/files
